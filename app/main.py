763f52e67ba71731277077278e4b9ef4d5f29e39
Jakub Lesniak <jlesniak03@icloud.com>
2026-06-18 11:58:28 +0100

    added frontend

--- app/main.py
+++ app/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI
 from app.db.session import engine
 from app.db import models
 from app.api.routes import accounts, auth, transfers
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def serve_frontend():
+    return FileResponse("frontend.html")
 
 models.Base.metadata.create_all(bind=engine)
 
